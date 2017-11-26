0207aa368ba5f69ee035c7f706475118c94e75cb
vanyadeg <id1997@yandex.ru>
2017-11-27 01:33:49 +0300

    chapter-2, task-3

---
+++ chapter2/task3.py
@@ -0,0 +1,33 @@
+from data.f2_matrix import F2Matrix
+
+
+class Order(object):
+    def __init__(self, comm):
+        params = comm.split()
+        self.command = params[0]
+        if len(params) > 1:
+            self.first = int(params[1])
+            self.second = int(params[2])
+
+
+if __name__ == '__main__':
+    matrix = F2Matrix([
+        [0, 1, 0, 1, 1, 0, 1, 0, 0, 0],
+        [1, 0, 1, 1, 1, 0, 0, 1, 0, 0],
+        [0, 1, 1, 0, 1, 1, 0, 0, 1, 0],
+        [1, 0, 1, 0, 1, 1, 0, 0, 0, 1]
+    ])
+
+    while True:
+        print matrix
+        order = Order(raw_input('Enter command:'))
+        if order.command == 'exit':
+            break
+        elif order.command == 'swap_c':
+            matrix.swap_columns(order.first, order.second)
+        elif order.command == 'swap_r':
+            matrix.swap_rows(order.first, order.second)
+        elif order.command == 'subtr_r':
+            matrix.subtract_row(order.first, order.second)
+        else:
+            raise Exception("Bad command")
---
+++ data/f2_matrix.py
@@ -0,0 +1,19 @@
+class F2Matrix(object):
+    def __init__(self, matrix):
+        self.matrix = matrix
+        self.height = len(matrix)
+        self.len = len(matrix[0])
+
+    def swap_columns(self, i, j):
+        for row in self.matrix:
+            row[i], row[j] = row[j], row[i]
+
+    def swap_rows(self, i, j):
+        self.matrix[i], self.matrix[j] = self.matrix[j], self.matrix[i]
+
+    def subtract_row(self, from_ind, which_ind):
+        zipped = zip(self.matrix[from_ind], self.matrix[which_ind])
+        self.matrix[from_ind] = map(lambda x: (x[0] + x[1]) % 2, zipped)
+
+    def __str__(self):
+        return '\n'.join([str(row) for row in self.matrix])
